af8366dfbde934778fd60def5e133c457e7b6e4a
Yahiaoui Rachid <r.yahiaoui.21@gmail.com>
2020-06-01 21:56:13 +0200

    Update CertMe.java

--- src/main/java/letsencrypt/CertMe.java
+++ src/main/java/letsencrypt/CertMe.java
@@ -785,10 +785,14 @@ public class CertMe {
                case "-jks"             :  jks             = true      ; break ;                
                case "-interface"       :  interfce        = args[i+1] ; break ;                
                case "-alias"           :  alias           = args[i+1] ; break ;               
-               case "-help"            :  printHelp()                 ; break ; 
+               case "-h"               :
+               case "-H"               :
                case "-port"            :  port            = Integer.parseInt  (
-                                                           args[i+1]) ; 
-                                                           break      ;                
+                                                            args[i+1]) ; 
+                                                            break      ;    
+               case "-help"            :  printHelp()     ; System.exit( 0)   ;
+                                                           break ;
+              
             }
         }
         
